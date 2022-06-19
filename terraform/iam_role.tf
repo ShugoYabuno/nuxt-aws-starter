2c3ff33120056d8d913df2bfa9c1ea55fde442e3
ShugoYabuno <shugo.ybn@gmai.com>
2022-06-19 17:51:40 +0900

    fix policy

--- terraform/iam_role.tf
+++ terraform/iam_role.tf
@@ -12,33 +12,19 @@ resource "aws_iam_policy" "codepipeline" {
   name   = "${var.project_name}-codepipeline-policy"
   policy = data.aws_iam_policy_document.codepipeline.json
 }
-
-# resource "aws_iam_policy" "codestar" {
-#   name = "${var.project_name}-codestar"
-
-#   policy = jsonencode({
-#     Version = "2012-10-17",
-#     Statement = [
-#       {
-#         Action = [
-#           "codestar-connections:UseConnection",
-#         ]
-#         Effect   = "Allow"
-#         Resource = aws_codestarconnections_connection.codestarconnections_connection.arn
-#       },
-#     ]
-#   })
-# }
+resource "aws_iam_policy" "codebuild" {
+  name   = "${var.project_name}-codebuild-policy"
+  policy = data.aws_iam_policy_document.codebuild.json
+}
 
 resource "aws_iam_role_policy_attachment" "codepipeline" {
   role       = aws_iam_role.codepipeline.id
   policy_arn = aws_iam_policy.codepipeline.arn
 }
-
-# resource "aws_iam_role_policy_attachment" "codestar" {
-#   role       = aws_iam_role.codepipeline.id
-#   policy_arn = aws_iam_policy.codestar.arn
-# }
+resource "aws_iam_role_policy_attachment" "codebuild" {
+  role       = aws_iam_role.codebuild.id
+  policy_arn = aws_iam_policy.codebuild.arn
+}
 
 data "aws_iam_policy_document" "codepipeline_assumerole" {
   statement {
@@ -64,9 +50,7 @@ data "aws_iam_policy_document" "codepipeline" {
   statement {
     effect = "Allow"
     actions = [
-      "s3:List*",
-      "s3:Get*",
-      "s3:PutObject"
+      "s3:*",
     ]
     resources = [
       aws_s3_bucket.pipeline_artifact.arn,
@@ -80,47 +64,36 @@ data "aws_iam_policy_document" "codepipeline" {
     ]
     resources = [aws_codestarconnections_connection.codestarconnections_connection.arn]
   }
+  statement {
+    effect = "Allow"
+    actions = [
+      "CodeBuild:*",
+    ]
+    resources = [aws_codebuild_project.main.arn]
+  }
 }
 
 data "aws_iam_policy_document" "codebuild" {
   statement {
     effect = "Allow"
     actions = [
-      "ecr:GetAuthorizationToken"
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
     ]
     resources = [
+      # aws_cloudwatch_log_group.codebuild.arn,
       "*"
     ]
   }
   statement {
     effect = "Allow"
     actions = [
-      "ecr:*"
-    ]
-    resources = [
-      aws_ecr_repository.ecr_repository.arn
-    ]
-  }
-  statement {
-    effect = "Allow"
-    actions = [
-      "s3:List*",
-      "s3:Get*",
-      "s3:PutObject"
+      "s3:*",
     ]
     resources = [
       aws_s3_bucket.pipeline_artifact.arn,
       "${aws_s3_bucket.pipeline_artifact.arn}/*"
-    ]
-  }
-  statement {
-    effect = "Allow"
-    actions = [
-      "logs:CreateLogStream",
-      "logs:PutLogEvents"
-    ]
-    resources = [
-      aws_cloudwatch_log_group.codebuild.arn,
     ]
   }
 }
