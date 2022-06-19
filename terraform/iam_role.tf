def89bd6e9b049930556469663e7ad978c4f3499
ShugoYabuno <shugo.ybn@gmai.com>
2022-06-19 20:10:38 +0900

    until build

--- terraform/iam_role.tf
+++ terraform/iam_role.tf
@@ -99,7 +99,7 @@ data "aws_iam_policy_document" "codebuild" {
   statement {
     effect = "Allow"
     actions = [
-      "ecr:GetAuthorizationToken",
+      "ecr:*",
     ]
     resources = ["*"]
   }
