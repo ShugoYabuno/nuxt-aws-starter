a91bc891f1d95a638e322c13e552d8f7a7feda92
ShugoYabuno <shugo.ybn@gmai.com>
2022-06-19 16:33:11 +0900

    merge file

--- terraform/cloud_watch.tf
+++ terraform/cloud_watch.tf
@@ -5,3 +5,7 @@ resource "aws_cloudwatch_log_group" "yada" {
     name = var.project_name
   }
 }
+
+resource "aws_cloudwatch_log_group" "codebuild" {
+  name = "/codebuild/nuxt-aws-starter-ecs-pipeline"
+}
