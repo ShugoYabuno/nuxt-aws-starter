7b455fc85baa22bdbfab85e625743e81122c2523
ShugoYabuno <shugo.ybn@gmai.com>
2022-06-19 19:39:37 +0900

    rename

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,5 +1,5 @@
 resource "aws_ecr_repository" "ecr_repository" {
-  name                 = "web_server/${var.project_name}"
+  name                 = var.project_name
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
